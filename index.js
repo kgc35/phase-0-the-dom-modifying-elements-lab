6e0948b062dd4c6bff927f095d2f1db06fbc8aa7
kent <kntchn@gmail.com>
2021-03-29 22:01:13 -0400

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+const text = document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(text);
+
+document.body.appendChild(newHeader);
+
+
